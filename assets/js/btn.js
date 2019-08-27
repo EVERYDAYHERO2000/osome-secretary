34c187765864e29582896535890c2f864bf895d8
EVERYDAYHERO2000 <ilyaflare@gmail.com>
2019-08-27 14:27:57 +0300

    fix

--- assets/js/btn.js
+++ assets/js/btn.js
@@ -11,6 +11,10 @@ $(function(){
   function detectBtnPos(){
     var $pos_1 = $('#pos_1');
     var $pos_2 = $('#pos_2'); 
+    var $pos_3 = $('#pos_3');
+    var $pos_4 = $('#pos_4');
+    var $pos_3_contain_1 = $('#pos_3_contain_1');
+    var $pos_3_contain_2 = $('#pos_3_contain_2');
     
     
     if ( $pos_2[0].getBoundingClientRect().top < $(window).outerHeight() ){
@@ -22,6 +26,37 @@ $(function(){
       $pos_2.addClass('btn_hidden');
       
     }
+    
+    if ( $pos_3[0].getBoundingClientRect().top < $(window).outerHeight() + 50  ) {
+      
+       
+       if ( !$pos_3.is('.btn_fixed') ) {
+         $pos_4.addClass('btn_hidden-cut').removeClass('btn_visible-cut');
+         $pos_3.addClass('btn_fixed');
+       }
+      
+      if ( $pos_3[0].getBoundingClientRect().top > $pos_3_contain_1[0].getBoundingClientRect().top &&
+           $pos_3[0].getBoundingClientRect().top < $pos_3_contain_2[0].getBoundingClientRect().top){ 
+      
+        
+        
+      } else {
+        
+        if ( $pos_3.is('.btn_fixed') ) {
+          $pos_4.removeClass('btn_hidden-cut').addClass('btn_visible-cut');
+          $pos_3.removeClass('btn_fixed');
+        }
+      }
+      
+    } else {
+       
+       if ( $pos_3.is('.btn_fixed') ) {
+         $pos_4.removeClass('btn_hidden-cut').addClass('btn_visible-cut');
+         $pos_3.removeClass('btn_fixed');
+       }
+    }
+    
+
   }
   
 });
