9177907181c9481bc291993f8ec58db613d30a55
EVERYDAYHERO2000 <ilyaflare@gmail.com>
2019-08-28 14:44:48 +0300

    Fix

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,7 +1,45 @@
 $(function(){
   
-  $('.slider').find('.slider__s').each(function(i,e){
-    
+  var slides = $('#slider').find('.slider__s').length;
+  var active = 0;
+  
+  var $prev = $('#slider').find('.slider__c-prev');
+  var $next = $('#slider').find('.slider__c-next');
+  var $container = $('#slider').find('.slider__inner');
+  
+  slide();
+  
+  $('#slider').find('.slider__c').click(function(){
+    slide( $(this) );
   });
   
+  function slide(control){
+    
+    if (control){
+       
+      if (control.is('.slider__c-prev') && active > 0){
+        active--;
+      }
+
+      if (control.is('.slider__c-next') && active < slides-1){
+        active++;
+      }
+    }
+    
+    $prev.removeClass('disabled');
+    $next.removeClass('disabled');
+    
+    if ( active == 0 ){
+      $prev.addClass('disabled');
+    } 
+    
+    if ( active == slides-1 ){
+      $next.addClass('disabled');
+    } 
+    
+    $container.css({
+      'margin-left' : '-' + (active * 100 ) + 'vw'
+    });
+    
+  }
 });
