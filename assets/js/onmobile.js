6106bc8b6899ebfa5fd20244dd6ae805988bc236
EVERYDAYHERO2000 <ilyaflare@gmail.com>
2019-08-31 22:25:51 +0300

    Fix

---
+++ assets/js/onmobile.js
@@ -0,0 +1,18 @@
+$(function(){
+  
+  $('body').addClass(window.browserDetect().isMobile);
+  
+  if ( $('body').is('.mobile') ){
+    
+    $('h1').html(`<strong>Paperless<br>Corporate<br>Secretary</strong>
+                your&nbsp;documents <strong>done&nbsp;right</strong>,
+                <strong>online</strong>,<br>and&nbsp;<strong>on&nbsp;time</strong>`);
+    
+    $('#cover_1').css({
+      'background-image' : 'url(assets/img/cover_1_mobile.png)'
+    })
+    
+  }
+  
+  
+});
