fc4f6c8999dad0a1f26513c3d5ba6eb8d3644633
EVERYDAYHERO2000 <ilyaflare@gmail.com>
2019-09-05 15:02:45 +0300

    Fix

---
+++ assets/js/webp.js
@@ -0,0 +1,15 @@
+$(function(){
+  
+  var hasWebP = false;
+  (function() {
+    var img = new Image();
+    img.onload = function() {
+      $('body').addClass('webp');
+    };
+    img.onerror = function() {
+      hasWebP = false;
+    };
+    img.src = '/assets/img/bizfile.webp';
+  })();
+  
+});
